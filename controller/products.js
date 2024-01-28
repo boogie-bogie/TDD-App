e386716ffb13a852aefb20c25210935562e5974a
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-01-28 15:05:58 +0900

    [TEST] Trouble shooting and Unit-test finished

--- controller/products.js
+++ controller/products.js
@@ -1,7 +1,11 @@
 const productModel = require("../schemas/products.schema");
 
 exports.createProduct = async (req, res, next) => {
-  const createdProduct = await productModel.create(req.body);
-  console.log("createdProduct", createdProduct);
-  res.status(201).json(createdProduct);
+  try {
+    const createdProduct = await productModel.create(req.body);
+    console.log("createdProduct", createdProduct);
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    next(error);
+  }
 };
